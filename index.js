3d98cd791f4b938363b3470ddc41120b404a2f1e
StephNdungu <stephy.wanjiku@gmail.com>
2024-07-03 16:49:51 +0300

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Mira Murati';
+
+//string concatenation
+
+console.log(welcomeMessage);
+
+//string interpolation
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+currentUser.slice(0,1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
